b77f01412a3fe3b8dbae44d2961470f38afb2eff
Viegas. <165096859+ViegasCode@users.noreply.github.com>
2026-01-13 14:02:19 -0300

    Insert default records into 'pessoas' table
    Add initial data to 'pessoas' table if empty

--- main.py
+++ main.py
@@ -35,6 +35,39 @@ def organograma():
     
     conn.commit()
 
+    conn = sqlite3.connect("database.db", check_same_thread=False)
+    c = conn.cursor()
+    
+    # Se a tabela já estiver criada, vamos garantir que só insira se estiver vazia
+    c.execute("SELECT COUNT(*) FROM pessoas")
+    if c.fetchone()[0] == 0:
+        pessoas = [
+            (1,"Fabio","Moreira","Coordenador","Diretoria",None),
+            (2,"Douglas","","Líder","Ongoing Tech",1),
+            (3,"Renan","","Dev","Ongoing Tech",2),
+            (4,"Jose","","Dev","Ongoing Tech",2),
+            (5,"Guilherme","","Líder","Observabilidade",1),
+            (6,"Diego","Miranda","Analista de Incidentes","Observabilidade",5),
+            (7,"Ronaldo","","Líder","Atendimento",1),
+            (8,"Keny","","Analista","Atendimento",7),
+            (9,"Diane","","Analista","Atendimento",7),
+            (10,"Ana Paula","","Analista","Atendimento",7),
+            (11,"Thiago","","Analista","Atendimento",7),
+            (12,"Marcos Vinicius","","Analista","Atendimento",7),
+            (13,"Matheus","Avila Paes","Líder","Office Suporte",1),
+            (14,"Marcelo","Ferreira de Abreu","Funcionário","Office Suporte",13),
+            (15,"Henrique","Ferreira Serafim","Funcionário","Office Suporte",13),
+            (16,"Magno","Alexandre Silva de Jesus","Funcionário","Office Suporte",13),
+            (17,"Adail","Pereira da Trindade","Funcionário","Office Suporte",13),
+            (18,"Vagner","Bezerra Oliveira","Terceiro","Office Suporte",13),
+            (19,"Flávio","Caetano","Terceiro","Office Suporte",13)
+        ]
+    
+        c.executemany("INSERT INTO pessoas (id,nome,sobrenome,cargo,setor,lider_id) VALUES (?,?,?,?,?,?)", pessoas)
+        conn.commit()
+    
+    conn.close()
+
     conn.row_factory = sqlite3.Row
     rows = conn.execute("SELECT * FROM pessoas").fetchall()
     conn.close()
